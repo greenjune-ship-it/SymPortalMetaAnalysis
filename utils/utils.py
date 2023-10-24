4f3c2de143a06182c0c6cc05f0b805ba0d088962
greenjune-ship-it <yulia_iakovleva@proton.me>
2023-10-24 17:37:01 +0200

    init commit

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 def read_datasheet(file_path):
     if file_path.endswith('.xlsx'):
-        return pd.read_excel(file_path, header=0, usecols='A:N', skiprows=[0])
+        return pd.read_excel(file_path, header=0, skiprows=[0])
     if file_path.endswith('csv'):
         return pd.read_csv(file_path)
